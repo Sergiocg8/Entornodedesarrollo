600fa2fd81d22b0b6597f29f0aa6d58414132707
Álvaro López Santamaría <58847640+alvaro2001lopez@users.noreply.github.com>
2020-02-13 19:59:59 +0100

    Update Persona.java

--- src/Persona.java
+++ src/Persona.java
@@ -1,3 +1,40 @@
- public class persona{
-	 
+ public class Persona{
+	 private String sNombre;
+	 private String sApellidos;
+	 private String sDNI;
+	 private int iEdad;
+	 private int iAntiguedad;
+	 public persona{
+		 
+	 }
+	public String getsNombre() {
+		return sNombre;
+	}
+	public void setsNombre(String sNombre) {
+		this.sNombre = sNombre;
+	}
+	public String getsApellidos() {
+		return sApellidos;
+	}
+	public void setsApellidos(String sApellidos) {
+		this.sApellidos = sApellidos;
+	}
+	public String getsDNI() {
+		return sDNI;
+	}
+	public void setsDNI(String sDNI) {
+		this.sDNI = sDNI;
+	}
+	public int getiEdad() {
+		return iEdad;
+	}
+	public void setiEdad(int iEdad) {
+		this.iEdad = iEdad;
+	}
+	public int getiAntiguedad() {
+		return iAntiguedad;
+	}
+	public void setiAntiguedad(int iAntiguedad) {
+		this.iAntiguedad = iAntiguedad;
+	}
  }
